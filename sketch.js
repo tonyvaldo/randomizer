9291b255c903cab88609a9665053abb36ee138c8
Tony <anthony.valdovinos@sjsu.edu>
2019-09-24 16:13:01 -0700

    roster

--- sketch.js
+++ sketch.js
@@ -1,8 +1,198 @@
+// class roster objects
+
+let roster = [{
+    firstName: "Zainib",
+    lastName: "Ahmed",
+    color: "Blue",
+    animal: "Giraffe",
+    book: "The Book Thief",
+    movie: "Amelie",
+    game: "none",
+    superhero: "The Hulk",
+    randomFact: "I love to eat olives."
+  },
+  {
+    firstName: "Toni",
+    lastName: "Anguiano",
+    favoriteColor: "blue",
+    favoriteAnimal: "cat",
+    favoriteMovieGenre: "Horror",
+    favoriteSuperHero: "batman",
+    randomFact: "I like to hangout at cemeteries and research dark California history"
+  },
+  {
+    firstName: "Camila",
+    lastName: "Avina Garcia",
+    favoriteColor: "Green",
+    favoriteAnimal: "Dog",
+    favoriteBook: "Match",
+    favoriteBook: "Match",
+    favoriteGame: " Guitar Hero",
+    favoriteSuperhero: "Spiderman",
+    randomFact: "I have a twin",
+  },
+  {
+    firstName: "Hector Flores",
+    color: "blue",
+    animal: "Lion",
+    book: "Captain underpants",
+    movie: "Ace Ventura pet detective",
+    game: "League Of Legends",
+    show: "Flash",
+    randomFact: "I'm a Libra"
+  },
+  {
+    firstName: "Eddson",
+    lastName: "Jose",
+    color: "midnight blue",
+    animal: "pig",
+    book: "The Dark Forest",
+    movie: "The Fountain",
+    game: "Halo Reach",
+    superhero: "Superman",
+    fact: "I am made up of stardust"
+  },
+  {
+    firstName: "Matteo",
+    lastName: "Leva",
+    color: "Black",
+    animal: "Cat": ,
+    book: "Pillars of the earth",
+    movie: "The Godfather",
+    game: "Witcher 3",
+    hero: "spider man",
+    randomFact: "I'm dissapointed in GOT seasoon 8"
+  },
+  {
+    firstName: "Lydia",
+    lastName: "Lim",
+    favoriteColor: "blue",
+    favoriteAnimal: "dog",
+    book: "No",
+    movie: "me before you",
+    game: "No",
+    superhero: "Thor",
+    randomFact: " birds are tetrapods even though they only walk on two legs"
+  },
+  {
+    firstName: "Chi",
+    lastName: "Nguyen",
+    favoriteColor: "purple",
+    animal: "bird",
+    book: "Math Curse",
+    movie: "Big Hero 6",
+    game: "Checkers",
+    superHero: "Big Hero 6 (team)",
+    randomFact: "Lately this year, monsters have especially been occupying my mind, among other things."
+  },
+  {
+    firstName: "Leon",
+    lastName: "Quach",
+    favoriteColor: "silver",
+    animal: "penguins",
+    book: "Letters to a Young Poet",
+    movie: "Kung fu Hustle",
+    game: "Pokemon Crystal Version",
+    superhero: "Batman",
+    randomFact: "I have two pet guinea pigs"
+  },
+  {
+    firstName: "Sierra",
+    lastName: "Roe",
+    favoriteColor: "purple",
+    favoriteAnimal: "dog",
+    favoriteBook: "phantom tollbooth",
+    favoriteMovie: "Naussica of the Valley of the Wind",
+    favoriteGame: "pokemon heartgold",
+    favoriteSuperhero: "captain marvel",
+    randomFact: "can run 7 minute mile"
+  },
+  {
+    firstName: "Hao",
+    lastName: "Rong",
+    favoriteColor: "blue",
+    animal: "otter",
+    book: "A Brief History of Time",
+    movie: "Angry Birds 2",
+    game: "Final Fantasy XIV",
+    superhero: "Tony Stark",
+    randomFact: "I like cats."
+  },
+  {
+    firstname: "Kaicong",
+    lastname: "Tang",
+    favColor: "black",
+    animal: "dog",
+    movie: "iron man123",
+    game: "csgo",
+    superhero: "iron man",
+    fact: "love gaming"
+  },
+  {
+    firstName: "Sumedh",
+    lastName: "Vedanthi",
+    faveColor: "Blue",
+    animal: "Crow",
+    book: "Of Mice and Men".movie: "American Psycho",
+    game: "Fruit Ninja",
+    superHero: "Batman",
+    randomFact: "im a black belt"
+  },
+  {
+    firstName: "Chris",
+    lastName: "Weber",
+    favoriteColor: "green",
+    favoriteAnimal: "cat",
+    favoriteBook: "Hitchhiker's Guide to the Galaxy",
+    favoriteMovie: "Spiderman Into The Spiderverse",
+    favoriteGame: "The Legend of Zelda Breath of the Wild",
+    favoriteSuperHero: "Iron Man",
+    randomFact: "I'm vegetarian"
+  },
+  {
+    firstName: "Andy",
+    lastName: "Wu",
+    favoriteColor: "Red",
+    animal: "Kiwi",
+    book: "Romance of the Three Kingdoms",
+    movie: "Shrek 2",
+    game: "Super Mario 64",
+    superhero: "Static Shock",
+    randomFact: "I've had jaw correction surgery"
+  },
+  {
+    firstName: "xiaowei",
+    lastName: "zheng",
+    favoriteColor: " green",
+    favoriteAnimal: "babies",
+    favoriteBook: "the little girl at the window",
+    favoriteMovie: "eat drink man woman",
+    favoriteGame: "none",
+    favoriteSuperHero: "none",
+    randomFact: "don't drink coke"
+  },
+  {
+    firstName: "Anthony",
+    lastName: "Valdovinos",
+    favoriteColor: "purple",
+    favoriteAnimal: "eagle",
+    favoriteBook: "Hatchet",
+    favoriteMovie: "Titanic",
+    favoriteGame: "Starfox Adventures",
+    favoriteSuperHero: "none",
+    randomFact: "I studied Japanese for 4 years"
+  }
+];
+
+
 function setup() {
   createCanvas(400, 400);
   background(150);
+
+  dogs.push("mimi")
+  console.log(dogs);
 }
 
-function draw(){
+function draw() {
 
 }
